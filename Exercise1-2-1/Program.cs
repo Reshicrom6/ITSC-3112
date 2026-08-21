73ca1f60d828e4732485a3df82ca2de744f63723
reshicrom6 <dunkanjd@gmail.com>
2026-08-21 13:00:23 -0400

    Used Accumulator in Program

--- Exercise1-2-1/Program.cs
+++ Exercise1-2-1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Accumulator accumulator = new Accumulator(10);
+        
+        Console.WriteLine(accumulator.Total);
+        
+        accumulator.Add(5);
+        Console.WriteLine(accumulator.Total);
     }
 }
