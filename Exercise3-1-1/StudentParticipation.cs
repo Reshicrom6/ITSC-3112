67a0edb5273a5bb8e3bb1a9e55e550b52cbe30ba
reshicrom6 <dunkanjd@gmail.com>
2026-09-02 13:06:45 -0400

    Enforce student participation invariants

---
+++ Exercise3-1-1/StudentParticipation.cs
@@ -0,0 +1,61 @@
+﻿namespace Exercise3_1_1;
+
+public class StudentParticipation
+{
+    public Student StudentInfo { get; }
+    public string ActionName { get; }
+    public int MaximumPoints { get; }
+    public int PointsEarned { get; private set; }
+
+    private void SetActionChoiceMapping(int actionChoice, out string actionName, out int maximumPoints)
+    {
+        switch (actionChoice)
+        {
+            case 1:
+                actionName = "Attended Class";
+                maximumPoints = 1;
+                break;
+            case 2:
+                actionName = "Attended office hours";
+                maximumPoints = 2;
+                break;
+            case 3:
+                actionName = "Answered Questions in Class";
+                maximumPoints = 3;
+                break;
+            case 4:
+                actionName = "Contributed to Canvas Discussion";
+                maximumPoints = 2;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(actionChoice));
+        }
+    }
+
+    public StudentParticipation(Student student, int actionChoice)
+    {
+        if (student is null)
+            throw new ArgumentNullException(nameof(student));
+
+        if (actionChoice < 1 || actionChoice > 4)
+            throw new ArgumentOutOfRangeException(nameof(actionChoice));
+
+        SetActionChoiceMapping(actionChoice, out string actionName, out int maximumPoints);
+
+        StudentInfo = student;
+        ActionName = actionName;
+        MaximumPoints = maximumPoints;
+        PointsEarned = 0;
+    }
+
+    public void UpdatePoints(int newPoints)
+    {
+        if (newPoints < 0 || newPoints > MaximumPoints)
+            throw new ArgumentOutOfRangeException(nameof(newPoints));
+        
+        PointsEarned = newPoints;
+    }
+    
+    
+    
+}
